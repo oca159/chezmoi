4bfbdc1a5bd59a19b046244ca5695d06ac494020
Osvaldo Cordova Aburto <ocordova@pulsarml.com>
2026-04-25 15:43:12 -0600

    feat: update nvim

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -9,32 +9,3 @@ autocmd("BufEnter", {
   pattern = "",
   command = "set fo-=c fo-=r fo-=o",
 })
-
--- Disable the concealing in some file formats
--- The default conceallevel is 3 in LazyVim
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
-  callback = function()
-    vim.wo.conceallevel = 0
-  end,
-})
-
-vim.api.nvim_create_user_command("Make", function(params)
-  -- Insert args at the '$*' in the makeprg
-  local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
-  if num_subs == 0 then
-    cmd = cmd .. " " .. params.args
-  end
-  local task = require("overseer").new_task({
-    cmd = vim.fn.expandcmd(cmd),
-    components = {
-      { "on_output_quickfix", open_height = 8, items_only = true, open_on_match = true },
-      "default",
-    },
-  })
-  task:start()
-end, {
-  desc = "Run your makeprg as an Overseer task",
-  nargs = "*",
-  bang = true,
-})
---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,98 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  keys = {
+    { "<leader>od", ":Obsidian dailies<cr>", desc = "obsidian [d]ailies" },
+    { "<leader>ot", ":e ~/notes/readme.md<cr>", desc = "obsidian [t]odo" },
+    { "<leader>ob", ":Obsidian backlinks<cr>", desc = "obsidian [b]acklinks" },
+    { "<leader>ol", ":Obsidian link<cr>", desc = "obsidian [l]ink selection" },
+    { "<leader>of", ":Obsidian follow_link<cr>", desc = "obsidian [f]ollow link" },
+    {
+      "<leader>onp",
+      function()
+        local title = vim.fn.input("Project: ")
+        if title ~= "" then
+          vim.cmd("Obsidian new_from_template 01 Projects/" .. title .. " template-project")
+        end
+      end,
+      desc = "obsidian [n]ew project from template",
+    },
+    {
+      "<leader>ona",
+      function()
+        local title = vim.fn.input("Area: ")
+        if title ~= "" then
+          vim.cmd("Obsidian new_from_template 02 Areas/" .. title .. " template-area")
+        end
+      end,
+      desc = "obsidian [n]ew area from template",
+    },
+    {
+      "<leader>onr",
+      function()
+        local title = vim.fn.input("Resource: ")
+        if title ~= "" then
+          vim.cmd("Obsidian new_from_template 03 Resources/" .. title .. " template-resource")
+        end
+      end,
+      desc = "obsidian [n]ew resource from template",
+    },
+    { "<leader>os", ":Obsidian search<cr>", desc = "obsidian [s]earch" },
+    { "<leader>oo", ":Obsidian quick_switch<cr>", desc = "obsidian [o]pen quickswitch" },
+    { "<leader>oO", ":Obsidian open<cr>", desc = "obsidian [O]pen in app" },
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in 4.0.0
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "05 Dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "template-daily",
+      workdays_only = false,
+    },
+    templates = {
+      subdir = "06 Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M:%S",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+    callbacks = {
+      pre_write_note = function(note)
+        -- This is called before a note is written to disk, you can use it to modify the note object.
+        -- For example, you could add a custom variable to the note like this:
+        -- note.custom_variable = "This is a custom variable"
+        note:add_field("updated", os.date("%Y-%m-%d %H:%M:%S"))
+        note:update_frontmatter()
+      end,
+    },
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+  },
+}
